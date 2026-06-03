1097cac4375d3ab9601b8c618ec38d1aea3be30e
2023-0521 <2023-0521@uteco.edu.do>
2026-06-03 17:30:06 -0400

    semana 2

--- obj/Debug/net10.0/LP3.BlazorServer.AssemblyInfo.cs
+++ obj/Debug/net10.0/LP3.BlazorServer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LP3.BlazorServer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bbc582074cd911356b1d56626506cfc9223b9f62")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f5ad3074741a853b0ae15b85f574e10772dafff3")]
 [assembly: System.Reflection.AssemblyProductAttribute("LP3.BlazorServer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LP3.BlazorServer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
